f74c03c19155e02c629a0994a65694184b819ec3
Tran Minh Tan <166855109+tanbaycu@users.noreply.github.com>
2025-01-19 02:30:23 +0000

    add 404 page

--- app.py
+++ app.py
@@ -185,7 +185,10 @@ def install_library():
             "error": "Installing libraries dynamically is not supported for security reasons.",
         }
     )
-
+    
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
